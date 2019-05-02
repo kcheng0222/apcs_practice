d0a3d2dcc948c1c4ec5df2a8bca427de1493fcd6
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-05-02 10:19:23 -0700

    Create c.java

---
+++ 2016/2/c.java
@@ -0,0 +1,15 @@
+public List<LogMessage> removeMessages(String keyword){
+
+	ArrayList<LogMessage> ans = new ArrayList<LogMessage>();
+	int i = 0;
+	while(i < messageList.size()){
+		if(messageList.get(i).containsWord(keyword)){
+			ans.add(messageList.remove(i));
+
+		}
+		else{
+			i += 1
+		}
+	}
+	return ans;
+}
