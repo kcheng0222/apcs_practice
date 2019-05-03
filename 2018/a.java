d3dca5512be86b2aafe565bd3d3f1a72a0076e91
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-05-03 09:22:57 -0700

    Create a.java

---
+++ 2018/a.java
@@ -0,0 +1,15 @@
+public boolean simulate(){
+    int distance = 0;
+
+    for(int i = 0; i < maxHops; i++){
+        distance += hopDistance();
+        if(distance >= goalDistance){
+            return true;
+        }
+        if(distance < 0){
+            return true;
+        }
+
+    }
+    return false;
+}
