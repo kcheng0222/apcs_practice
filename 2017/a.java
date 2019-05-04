5c44e3820144b2180d74e5e41658dc41b82c7d8c
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-05-04 09:20:30 -0700

    Update a.java

--- 2017/a.java
+++ 2017/a.java
@@ -1,1 +1,13 @@
+public Digits(int num){
+    digitList = new ArrayList<Integer>;
 
+    if(num == 0){ // special case
+        digitList.add(0);
+    }
+    else{
+        while(num > 0){
+            digitList.add(0, num % 10);
+            num = num / 10;
+        }
+    }
+}
