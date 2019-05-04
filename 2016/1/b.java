861ec603d4b393a4a49b506bfb518fc589c3db4a
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-05-04 08:07:40 -0700

    Don't need that

--- 2016/1/b.java
+++ 2016/1/b.java
@@ -1,14 +1,7 @@
 public class RandomLetterChooser extends RandomStringChooser{
-
+    
     public RandomLetterChoose(String str){
-      super(getSingleLetters(str));
+        super(getSingleLetters(str));
     }
     
-    public String[] getSingleLetters(String str){
-        String[] arr = new String[str.length()];
-        for(int i = 0; i < str.length; i++){
-            arr[i] = str.substring(i, i+1);
-        }
-        return arr;
-    }
 }
