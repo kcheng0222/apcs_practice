3391a11021d687eca56b0f8c01d12a5ad33d747c
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-05-04 09:07:11 -0700

    Changed b/c it added extra gaps at end

--- 2016/4/c.java
+++ 2016/4/c.java
@@ -1,18 +1,22 @@
 public static String format(List<String> wordList, int formattedLen){
     String ans = "";
+    String gap = "";
 
-    int gap = basicGapWidth(wordList, formattedLen);
+    int gapLength = basicGapWidth(wordList, formattedLen);
     int extra = leftoverSpaces(wordList, formattedLen);
+    
+    for(int x = 0; x < gapLength; x++){
+        gap += " ";   
+    }
 
-    for(String x : wordList){
-        ans += x;
-        for(int y = 0; y < gap; y++){
-            ans += " ";
-        }
+    for(int x = 0; x < wordList.size() - 1; x++){
+        ans += wordList.get(x) + gap;
         if(extra > 0){
             ans += " ";
             extra -= 1;
         }
     }
+    ans += wordList.get(wordList.size() - 1);
+
     return ans;
 }
