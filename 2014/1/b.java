fc8ef5b92bc220a5695a82996087c64443979286
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-05-07 12:48:04 -0700

    Create b.java

---
+++ 2014/1/b.java
@@ -0,0 +1,13 @@
+public static void scrambleOrRemove(List<String> wordList){
+    int i = 0;
+    
+    while(i < wordList.length){
+        String x = wordList.get(i);
+        String y = scrambleWord(x);
+        if(!x.equals(y)){
+            wordList.set(i, y);
+            i += 1;
+        }
+    }
+    return wordList;
+}
