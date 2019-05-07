1e16991f8370ea81b61b0626133cfc583f857577
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-05-07 10:09:39 -0700

    Update a.java

--- 2015/2/a.java
+++ 2015/2/a.java
@@ -1,1 +1,25 @@
-
+public class HiddenWord{
+    
+    private String word;
+    
+    public HiddenWord(String s){
+        word = s;
+    }
+    
+    public getHint(String guess){
+        String result = "";
+        
+        for(int x = 0; x < guess.length(); x++){
+            if(word.substring(x, x+1).equals(guess.substring(x, x+1))){
+                result += word.substring(x, x+1);
+            }
+            else if(word.contains(guess.substring(x, x+1))){
+                    result += "+";
+                }
+            else{
+                result += "*";
+            } 
+        }
+        return result;
+    }
+}
