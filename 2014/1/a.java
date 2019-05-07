daeb2260a0b3e9fab71879e95c52125009e4e85f
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-05-07 12:07:05 -0700

    Update a.java

--- 2014/1/a.java
+++ 2014/1/a.java
@@ -1,1 +1,23 @@
-
+public static String scrambleWord(String word){
+    String ans = "";
+    
+    if(word.length() <= 1){
+        return word;
+    }
+    
+    int i = 0;
+    
+    while(i < word.length() - 1){
+        String a = word.substring(i, i+1);
+        String b = word.substring(i+1, i+2);
+        if(a.equals("A") && !b.equals("A")){
+            ans += b + a;
+            i += 2;
+        }
+        else{
+            ans += a;
+            i += 1
+        }
+    }
+    return ans;
+}
