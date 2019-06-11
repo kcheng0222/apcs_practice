c27bb3d5801223c8cf53e86038d1991e3cc77eed
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-06-10 22:16:17 -0700

    Update E13-12.java

--- recursion/E13-12.java
+++ recursion/E13-12.java
@@ -1,25 +1,22 @@
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class SubsetGenerator{    
+class Ideone
+{   
     public static List<String> calc(String s, int start, int end, List<String> arr){
     	System.out.println(arr);
     	arr.add(s.substring(start, end));
-        if(end > s.length() - 1 && start == end){
-        	System.out.println("Exiting");
-            return arr;
+        
+        if(s.length() == 1){
+        	return arr;
         }
-        if(end == s.length() - 1){
-            end = start += 2;
-            start += 1;
-        }
-        else{
-            end += 1;
-        }
+        end ++;
 
-        return calc(s, start, end, arr);
+        return calc(s.substring(1), start, end, arr);
     }
+    
     public static void main(String [] args){
         String s = "Why are we doing this";
         s = "rum";
