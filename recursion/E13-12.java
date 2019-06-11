c13e8c844590168ffd9504e2cb3b019286ca1a3d
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-06-10 22:25:26 -0700

    Update E13-12.java

--- recursion/E13-12.java
+++ recursion/E13-12.java
@@ -3,24 +3,26 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Ideone
+class SubstringGenerator
 {   
-    public static List<String> calc(String s, int start, int end, List<String> arr){
+    public static List<String> calc(String s, List<String> arr){
     	System.out.println(arr);
-    	arr.add(s.substring(start, end));
         
-        if(s.length() == 1){
+        if(s.length() < 1){
         	return arr;
         }
-        end ++;
+        
+        for(int i = 1; i <= s.length(); i++){
+        	arr.add(s.substring(0, i));
+        }
 
-        return calc(s.substring(1), start, end, arr);
+        return calc(s.substring(1), arr);
     }
     
     public static void main(String [] args){
         String s = "Why are we doing this";
         s = "rum";
         System.out.println(s);
-        System.out.println(calc(s, 0, 1, new ArrayList<String>()));
+        System.out.println(calc(s, new ArrayList<String>()));
     }
 }
