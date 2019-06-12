9a81dcf9d5272c31dac8346e41557ad2474de60e
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-06-11 20:52:50 -0700

    Update E13-6.java

--- recursion/E13-6.java
+++ recursion/E13-6.java
@@ -1,9 +1,19 @@
-public static boolean find(String text, String str){
-    if(text.startsWith(str)){
-        return true;
+public class Finder{
+    public static boolean find(String text, String str){
+	    if(text.startsWith(str)){
+	        return true;
+	    }
+	    if(text.length() < str.length()){
+	        return false;
+	    }
+	    return find(text.substring(1), str);
     }
-    if(text.length() < str.length()){
-        return false;
+    
+    public static void main(String [] args){
+    	System.out.println(find("apple", "apple"));
+    	System.out.println(find("xapplex", "apple"));
+    	System.out.println(find("apple", "xapplex"));
+    	System.out.println(find("apple", "appl"));
+    	System.out.println(find("x", "apple"));
     }
-    return find(text.substring(1), str);
 }
