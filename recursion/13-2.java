d5d2d19c6581a62429394ad15e5e08c502b4c132
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-06-11 20:39:28 -0700

    Update 13-2.java

--- recursion/13-2.java
+++ recursion/13-2.java
@@ -1,13 +1,13 @@
 public class Tester{
-    public String foo(String s, int i){
+    public static String foo(String s, int i){
         if(i >= s.length()){
             return s;
         }
-        return foo(s.subtring(1, s.length()), i) + s.substring(0, 1);
+        return foo(s.substring(1, s.length()), i) + s.substring(0, 1);
     }
     public static void main(String [] args){
-        System.out.println(foo("gummy bears"), 5);
-        System.out.println(foo("gummy bears"), 7);
-        System.out.println(foo("gummy bears"), 1);
+        System.out.println(foo("gummy bears", 5));
+        System.out.println(foo("gummy bears", 7));
+        System.out.println(foo("gummy bears", 1));
     }
 }
