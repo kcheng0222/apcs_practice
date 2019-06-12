0d1f0422ed4007a1e6a21c734f615a88cbc03a7d
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-06-11 17:49:16 -0700

    Create 13-13.java

---
+++ recursion/13-13.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class SubstringGenerator
+{   
+    public static List<String> calc(String s, List<String> arr){
+        System.out.println(arr);
+        
+        if(s.length() < 1){
+            return arr;
+        }
+        
+        for(int i = 1; i <= s.length(); i++){
+            arr.add(s.substring(0, i));
+        }
+        return calc(s.substring(1), arr);
+    }
+    
+    public static void main(String [] args){
+        String s = "Why are we doing this";
+        s = "rum";
+        System.out.println(s);
+        System.out.println(calc(s, new ArrayList<String>()));
+    }
+}
