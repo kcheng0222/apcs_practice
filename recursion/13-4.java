5a7afbd96e55c74178f5336ed97fe91e48eb6a62
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-06-11 20:43:55 -0700

    Update 13-4.java

--- recursion/13-4.java
+++ recursion/13-4.java
@@ -1,13 +1,13 @@
 public class Choose{
-    public int nChooseK(int n, int k){
+    public static int nChooseK(int n, int k){
         if(n == 0 || n == k){
             return 1;
         }
         return nChooseK(n-1, k) + nChooseK(n-1, k-1);
     }
     public static void main(String [] args){
-        System.out.println(52, 13);
-        System.out.println(6, 2);
-        System.out.println(10, 10);
+        System.out.println(nChooseK(52, 13));
+        System.out.println(nChooseK(6, 2));
+        System.out.println(nChooseK(10, 10));
     }
 }
