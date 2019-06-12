6df7cbd2bf72a19c7baad147638bd04af4a3c0b6
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-06-11 20:14:53 -0700

    Update 13-40.java

--- recursion/13-40.java
+++ recursion/13-40.java
@@ -1,6 +1,16 @@
-public String reverse(String s){
-    if(s.length() == 1){
-        return s;
+public class Test{
+    public String reverse(String s){
+        if(s.length() == 1){
+            return s;
+        }
+        return s.substring(s.length() - 1, s.length() - 2) + reverse(s.substring(0, s.length() - 1));
     }
-    return s.substring(s.length() - 1, s.length() - 2) + reverse(s.substring(0, s.length() - 1));
+    
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string!");
+        String s = sc.nextLine();
+        System.out.println(s);
+        System.out.println("After: " + reverse(s));
+    }
 }
