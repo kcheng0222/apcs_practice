3f99364d21960e199f0597ae2c27e669742d7903
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-06-11 20:33:11 -0700

    Update 13-1.java

--- recursion/13-1.java
+++ recursion/13-1.java
@@ -1,7 +1,7 @@
 public class Tester{
     public static int gcd(int a, int b){
-        if(a == 0 || b= == 0){
-            return b;
+        if(b == 0){
+            return a;
         }
         return gcd(b, a % b);
     }
