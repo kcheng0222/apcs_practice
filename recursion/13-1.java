e775bc15ca236b3e54b170fdf62a61a76c72a9b1
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-06-11 17:21:26 -0700

    Update 13-1.java

--- recursion/13-1.java
+++ recursion/13-1.java
@@ -1,1 +1,14 @@
-//gcd
+public class Tester{
+    public int gcd(int a, int b){
+        if(a == 0){
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+    public static void main(String [] args){
+        System.out.println(gcd(8, 4));
+        System.out.println(gcd(4, 8));
+        System.out.println(gcd(8, 8));
+        System.out.println(gcd(0, 4));
+    }
+}
